414a6d085f37aba2f0bfd544477a4e76db3bdf2f
Tananonthiphat <tananonthiphat.m@kkumail.com>
2024-06-27 17:35:52 +0700

    maibok

--- src/@layouts/components/VerticalNav.vue
+++ src/@layouts/components/VerticalNav.vue
@@ -66,7 +66,7 @@ const handleNavScroll = evt => {
           />
 
           <h1 class="font-weight-medium leading-normal text-xl text-uppercase">
-            Materio
+            Maibok
           </h1>
         </RouterLink>
       </slot>
